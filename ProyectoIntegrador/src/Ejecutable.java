26492919fdc208648a4a7024901b203392ac9e1d
Christopher <chris.97e@gmail.com>
2017-03-06 10:25:18 -0500

    Ejecutable <(CREACIÓN)>

---
+++ ProyectoIntegrador/src/Ejecutable.java
@@ -0,0 +1,31 @@
+import processing.core.PApplet;
+
+public class Ejecutable {
+	
+    public static PApplet app;
+    
+	public static void main(String[] args) {
+	
+       PApplet.main("Ejecutable");
+	}
+	
+	
+	public void setting(){
+		
+	}
+	
+	public void setup(){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+	public void keyPressed(){	
+		
+	}
+	
+	
+
+}
