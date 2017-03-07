6f9eac653a2afc874cf75723b828d3c1739e1aab
estudiante <estudiante@192.168.108.18>
2017-03-06 21:43:57 -0500

    hsdufahsufg

--- ProyectoIntegrador/src/Logica.java
+++ ProyectoIntegrador/src/Logica.java
@@ -1,13 +1,29 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Logica {
 
 	private int pantalla = 1;
 	private PApplet app;
+	ArrayList lista = new ArrayList<Personas>();
 
 	public Logica(PApplet app) {
+		String[] nombres, datos, colores;
 		this.app=app;
 
+		nombres = app.loadStrings("data/nombres.txt");
+		datos = app.loadStrings("data/datos.txt");
+		colores = app.loadStrings("data/colores.txt");	
+		
+		for (int i = 0; i < nombres.length; i++) {
+			String[] nombre = nombres[i].split(":");
+			String[] dato = datos[i].split("/");
+			String[] color = colores[i].split("/");
+			lista.add(new Personas(nombre[0],nombre[1],Integer.parseInt(dato[0]),Integer.parseInt(dato[1]),Integer.parseInt(dato[2]),Integer.parseInt(color[0]),Integer.parseInt(color[1]),Integer.parseInt(color[2])));
+		}
+		
+		app.println(lista.size());
 	}
 
 	public void pantalla() {
--- ProyectoIntegrador/src/Personas.java
+++ ProyectoIntegrador/src/Personas.java
@@ -1,13 +1,60 @@
 
 public class Personas {
 	
+	public Personas(String nombre, String apellido, int cédula, int edad, int peso, int r, int g, int b) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.cédula = cédula;
+		this.edad = edad;
+		this.peso = peso;
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+
+
 	private String nombre, apellido;
 	private int cédula, edad, peso, r, g, b;
 
 	
 	
-	public Personas( String el){
-		
+
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personas other = (Personas) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
 	}
 	
 	
